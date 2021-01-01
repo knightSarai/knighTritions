86cd4925095319841972386621da416bc970570c
knightSarai <knight.sarai.dev@gmail.com>
2021-01-01 20:50:47 +0200

    add google and email actions and its reducers

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,17 +1,32 @@
-import {SET_CURRENT_USER} from './user.types'
+import {
+    GOOGLE_SIGN_IN_FAILURE,
+    GOOGLE_SIGN_IN_SUCCESS, 
+    EMAIL_SIGN_IN_FAILURE,
+    EMAIL_SIGN_IN_SUCCESS
+} from './user.types';
+
 const INITIAL_STATE = {
-    currentUser: null
+    currentUser: null,
+    err: null
 }
 
 const userReducer =  (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case SET_CURRENT_USER :
+        case GOOGLE_SIGN_IN_SUCCESS :
+        case EMAIL_SIGN_IN_SUCCESS:
             return {
                 ...state,
-                currentUser: action.payload
-            }
+                currentUser: action.payload,
+                err: null
+            };
+        case GOOGLE_SIGN_IN_FAILURE :
+        case EMAIL_SIGN_IN_FAILURE:
+            return {
+                ...state,
+                err: null
+            };
         default:
-            return state
+            return state;
     }
 }
 
