13b2b4ec0ab3e13d833a2cd57deb624b05450d84
knightSarai <knight.sarai.dev@gmail.com>
2021-01-12 21:31:47 +0200

    convert sign in form

--- src/components/forms/Sign-in/index.jsx
+++ src/components/forms/Sign-in/index.jsx
@@ -1,56 +1,55 @@
-import React from 'react';
-import {withRouter} from 'react-router-dom';
-import {connect} from 'react-redux';
-import {googleSignInStart, emailSignInStart} from '../../../redux/user/user.action';
+import React, { useState } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { googleSignInStart, emailSignInStart } from '../../../redux/user/user.action';
 import Input from '../Form-input'
 import SignIn from './sign-in.styles';
 import Button from '../Form-button';
 
-class SignInForm extends React.Component{
-    state = {
+const SignInForm = ({ googleSignInStart, emailSignInStart }) => {
+    const initialState = {
         email: '',
         password: ''
     }
-    handleChange = evt => {
-        const {name, value} = evt.target;
-        this.setState({
-            ...this.state,
-            [name]: value 
+
+    const [userCredentials, setUserCredentials] = useState(initialState);
+    const { email, password } = userCredentials;
+    const handleChange = evt => {
+        const { name, value } = evt.target;
+        setUserCredentials({
+            ...userCredentials,
+            [name]: value
         })
     }
 
-    handleSubmit = async evt => {
+    const handleSubmit = async evt => {
         evt.preventDefault();
-        const {emailSignInStart} =  this.props;
-        const {email, password} = this.state;
-        emailSignInStart({email, password});
-        
-    }
-    render () {
-        const {googleSignInStart} = this.props;
+        const { email, password } = userCredentials;
+        emailSignInStart({ email, password });
 
-        return (
-            <SignIn >
-                <h1>I Already Have An Account</h1>
-                <span>Sign in with your email & password</span>
-                <form onSubmit={this.handleSubmit}>
-                    <Input label="Email" type="email" name="email" value={this.state.email} required handleChange={this.handleChange}/>
-                    <Input label="Password" type="password" name="password" value={this.state.password} required onChange={this.handleChange}/>
-                    <div className="buttons">
-                        <Button type="submit">Sign in</Button>
-                        <Button 
-                            type="button" 
-                            onClick={googleSignInStart} 
-                            isGoogleSignIn
-                        >
-                                Sign in with Google
+    }
+
+
+    return (
+        <SignIn >
+            <h1>I Already Have An Account</h1>
+            <span>Sign in with your email & password</span>
+            <form onSubmit={handleSubmit}>
+                <Input label="Email" type="email" name="email" value={email} required handleChange={handleChange} />
+                <Input label="Password" type="password" name="password" value={password} required onChange={handleChange} />
+                <div className="buttons">
+                    <Button type="submit">Sign in</Button>
+                    <Button
+                        type="button"
+                        onClick={googleSignInStart}
+                        isGoogleSignIn
+                    >
+                        Sign in with Google
                         </Button>
-                    </div>
-                </form>
-            </SignIn>
-        )
-    }
-
+                </div>
+            </form>
+        </SignIn>
+    )
 }
 
-export default connect(null, {googleSignInStart, emailSignInStart})(withRouter(SignInForm));
+export default connect(null, { googleSignInStart, emailSignInStart })(withRouter(SignInForm));
